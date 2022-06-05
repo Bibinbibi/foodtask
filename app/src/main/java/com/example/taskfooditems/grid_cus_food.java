0d04049bd051f597b358076436c8330b4ea94210
Bibinbibi <bibinbalan725@gmail.com>
2022-06-05 14:33:55 +0530

    commit

---
+++ app/src/main/java/com/example/taskfooditems/grid_cus_food.java
@@ -0,0 +1,14 @@
+package com.example.taskfooditems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class grid_cus_food extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grid_cus_food);
+    }
+}
